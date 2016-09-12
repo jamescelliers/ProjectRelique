f1e6eefba32ca7da801e2d169925127d728c4d5f
jamescelliers <jamescelliers@hotmail.co.uk>
2016-09-12 02:28:12 +0100

    Refactored

--- ProjectRelique_Engine/Assets/Items/Item.cs
+++ ProjectRelique_Engine/Assets/Items/Item.cs
@@ -5,9 +5,13 @@ public class Item : MonoBehaviour
 {
 
     public string Name;
+    private bool dropped;
+
     public Sprite EquippedSprite;
     public Sprite DroppedSprite;
-    private bool dropped;
+    public Sprite[] UseAnimationSprites;
+    public int DefaultSpriteIndex;
+
     public bool Dropped
     {
         get { return dropped; }
@@ -15,33 +19,24 @@ public class Item : MonoBehaviour
         {
             if (!value)
             {
+                //Pickup
 
             }
             else
             {
-                itemCollider.size = ColliderSize;
+                //Drop
                 transform.SetParent(null);
                 gameObject.AddComponent<SpriteRenderer>().sprite = DroppedSprite;
             }
             dropped = value;
         }
     }
-    public Vector2 ColliderSize;
-    public BoxCollider2D itemCollider { get; private set; }
-
-    void Awake()
-    {
-        itemCollider = gameObject.AddComponent<BoxCollider2D>();
-        itemCollider.isTrigger = true;
-        itemCollider.size = ColliderSize;
-        itemCollider.offset = new Vector2( 0.0f, ColliderSize.y / 2);
-    }
 
     public virtual void Use()
     {
         if (!dropped)
         {
-            print("Used Item");
+            print("Tried To Use Item (Does nothing, is it Empty?)");
         }
     }
 
