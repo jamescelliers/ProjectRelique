3e95bdfdf45858628d0ba9c3d0e58de0d631b6f4
jamescelliers <jamescelliers@hotmail.co.uk>
2016-09-12 02:27:25 +0100

    Destory After Time

---
+++ ProjectRelique_Engine/Assets/HitCollider.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitCollider : MonoBehaviour {
+
+    public float TimeAlive = 1f;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(DestroyAfterTime());
+	}
+
+    IEnumerator DestroyAfterTime()
+    {
+        yield return new WaitForSeconds(TimeAlive);
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        print("Hit " + coll.gameObject.name);
+    }
+}
