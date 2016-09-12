a71a731d83f753ff284acac0aa7677f597ecc2c3
jamescelliers <jamescelliers@hotmail.co.uk>
2016-09-12 02:26:22 +0100

    Added GetOtAddComponent<>()

--- ProjectRelique_Engine/Assets/EngineExtensions/ExtensionMethods.cs
+++ ProjectRelique_Engine/Assets/EngineExtensions/ExtensionMethods.cs
@@ -27,4 +27,18 @@ public static class ExtensionMethods
         objectToRename.name = s;
     }
 
+        /// <summary>
+        /// Gets or add a component. Usage example:
+        /// BoxCollider boxCollider = transform.GetOrAddComponent<BoxCollider>();
+        /// </summary>
+        public static T GetOrAddComponent<T>(this Component child) where T : Component
+        {
+            T result = child.GetComponent<T>();
+            if (result == null)
+            {
+                result = child.gameObject.AddComponent<T>();
+            }
+            return result;
+        }
+
 }
