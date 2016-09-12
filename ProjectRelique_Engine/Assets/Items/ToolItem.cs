9768869db6556fa746d3b96d563331f01b1a138f
jamescelliers <jamescelliers@hotmail.co.uk>
2016-09-12 02:28:37 +0100

    Extended Item

---
+++ ProjectRelique_Engine/Assets/Items/ToolItem.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToolItem : Item
+{
+
+    public enum ToolType { Melee, Projectile }
+    public ToolType ToolItemType;
+    public GameObject HitColliderPrefab;
+
+    private bool canFire = true;
+    public float FireRate = 1f;
+    private float fireRateTimer = 0f;
+
+    void Update()
+    {
+        if (fireRateTimer > 0)
+        {
+            fireRateTimer -= Time.deltaTime;
+        }
+        else
+        {
+            fireRateTimer = 0;
+            canFire = true;
+        }
+    }
+
+    public override void Use()
+    {
+        CreateHitObject();
+
+    }
+
+    private void CreateHitObject()
+    {
+        if (canFire)
+        {
+            if (ToolItemType == ToolType.Melee)
+            {
+                GameObject hcInstance = (GameObject) Instantiate(HitColliderPrefab, 
+                   transform.position, transform.rotation);
+                hcInstance.transform.SetParent(transform);
+                canFire = false;
+            }else
+            {
+
+            }
+
+            fireRateTimer = FireRate;
+        }
+    }
+
+}
